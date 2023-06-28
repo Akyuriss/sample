243f09b7d01cfaec3a6aa27da96588828b051765
Akyuriss <135511100+Akyuriss@users.noreply.github.com>
2023-06-28 21:08:35 +0900

    最初からやってから原因追及

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,15 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
-@RestController
+
+@Controller
 public class AppController {
 
-//	@GetMapping("/index")
-	@RequestMapping("/")
+	@GetMapping("/")
+//	@RequestMapping("/")
 	public String index() {
 		return "index";
 }
