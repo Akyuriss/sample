6c6c66010ba2986860aa3a731e8d2a2b8a1a9ba2
Akyuriss <135511100+Akyuriss@users.noreply.github.com>
2023-06-28 21:00:04 +0900

    途中段階

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-@Controller
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class AppController {
 
-	@GetMapping("/index")
+//	@GetMapping("/index")
+	@RequestMapping("/")
 	public String index() {
 		return "index";
 }
